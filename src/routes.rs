1ed50d9fd9f339152af7d26cb5aff79840fb4f15
Tanguy Soubry <tanguy.soubry@edpnet.be>
2025-05-03 11:37:11 +0200

    keep simple

--- src/routes.rs
+++ src/routes.rs
@@ -1,26 +1,35 @@
-use axum::response::{Html, IntoResponse};
-use axum_htmx::{HxResponseTrigger, HxTrigger};
+use axum::{http::StatusCode, response::Html};
+use axum_htmx::{HxEvent, HxRequest, HxResponseTrigger};
 use maud::html;
+use serde_json::json;
 
 use crate::templates::render_index;
 
 pub const CLICK: &str = "click";
 
 pub async fn index() -> (HxResponseTrigger, Html<String>) {
-    (HxResponseTrigger::normal([CLICK]), render_index().await)
+    let event = HxEvent::new_with_data(
+        "my-event",
+        // May be any object that implements `serde::Serialize`
+        json!({"level": "info", "message": {
+            "title": "Hello, world!",
+            "body": "This is a test message.",
+        }}),
+    )
+    .unwrap();
+
+    (HxResponseTrigger::normal([event]), render_index().await)
 }
 
-pub async fn clicked(HxTrigger(trigger): HxTrigger) -> impl IntoResponse {
-    let result = match trigger {
-        Some(event) => event,
-        _ => "Error!".to_string(),
-    };
+pub async fn clicked(HxRequest(clicked): HxRequest) -> (StatusCode, Html<String>) {
+    if clicked {
+        let markup = html!(
+            {
+                p { "clicked" }
+            }
+        );
+        return (StatusCode::OK, Html(markup.into_string()));
+    }
 
-    let markup = html!(
-        {
-            p { "Event was: " (result) }
-        }
-    );
-
-    Html(markup.into_string())
+    (StatusCode::NOT_FOUND, Html("404 Not Found".to_string()))
 }
