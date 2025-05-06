1f4094b5ba2da9f4de00950d6eb6758b1c608ef8
Tanguy Soubry <tanguy.soubry@edpnet.be>
2025-05-06 12:53:27 +0200

    add rust helmet

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 This project is an exploration of the MASH stack in Rust, inspired by [Evan Schwartz's guide](https://emschwartz.me/building-a-fast-website-with-the-mash-stack-in-rust/). The goal is to combine the best practices for building fast, secure, and maintainable web applications using Rust.
 
 By leveraging "supersafe Rust" principles, this project aims to enforce stricter safety and security measures, ensuring robust and reliable code while maintaining high performance.
+
+## todo
+
+- add <https://docs.rs/axum-extra/latest/axum_extra/index.html>
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::Router;
+use axum_helmet::{Helmet, HelmetLayer};
 use axum_htmx::AutoVaryLayer;
 use routing::main_router;
 use serde_json as _;
@@ -12,7 +13,10 @@ const PORT: u16 = 8080;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let app = Router::new().merge(main_router()).layer(AutoVaryLayer);
+    let app = Router::new()
+        .merge(main_router())
+        .layer(AutoVaryLayer)
+        .layer(HelmetLayer::new(Helmet::default()));
 
     let listener = tokio::net::TcpListener::bind(format!("[::]:{PORT}")).await?;
     println!("Server running at http://localhost:{}", PORT);
